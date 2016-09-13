218b9bb84edd0a04b0e265e6d25039cfb52c7cc6
zhengyu92 <leowzhengyu@hotmail.com>
2016-09-13 19:18:32 +0800

    Add abstact method 'isMutating()' to Command.java

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,11 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * @return true for command types that mutate data, false otherwise
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
